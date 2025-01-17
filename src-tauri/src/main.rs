be1254c20593e7aae61baccca12c007911e09761
jesee <jesee520@gmail.com>
2025-01-17 12:07:54 +0800

    simple demo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn start_server() -> String {
+    println!("Server starting...");
+    "ok".to_string()
+}
+
+#[tauri::command]
+fn stop_server() -> String {
+    println!("Server stopping...");
+    "ok".to_string()
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 fn main() {
-    tauri_app_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![start_server, stop_server])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
